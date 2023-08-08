c19a14a2e8d2345d83908c386c8c77e23baa12ed
Vu <mac@Macs-MacBook-Pro.local>
2023-08-08 11:21:05 +0700

    test merge

--- src/main/java/com/example/vtiproject/VtiProjectApplication.java
+++ src/main/java/com/example/vtiproject/VtiProjectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VtiProjectApplication {
+	//test merge
 	public static void main(String[] args) {
 		SpringApplication.run(VtiProjectApplication.class, args);
 	}
